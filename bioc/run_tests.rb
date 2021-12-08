6ca7f5b18e448b3a41fef0555c73978166b90da3
kojix2 <2xijok@gmail.com>
2021-12-08 10:57:12 +0900

    Add run_test

--- bioc/run_tests.rb
+++ bioc/run_tests.rb
@@ -1,15 +1,21 @@
 require "json"
-require "fileutils"
 
 package_list_path = File.expand_path("packages.json", __dir__)
 output_dir = File.expand_path("packages", __dir__)
-FileUtils.mkdir_p(output_dir)
 
 package_metadata = JSON.parse(File.read(package_list_path))
 
+result = {}
+
 package_metadata.each do |metadata|
   name    = metadata["Package"]
-  version = metadata["Version"]
-  md5     = metadata["MD5sum"]
-  system "biocgem new -n #{name} -v #{version} -m #{md5} -o #{output_dir}"
+
+  Dir.chdir(File.join(output_dir, name)) do
+    system "bundle config set --local path 'vendor/bundle'"
+    system "bundle install"
+    system "bundle exec rake extdata:download"
+    result[name] = system "bundle exec rake test"
+  end 
 end
+
+pp result
